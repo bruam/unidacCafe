518c7bbec65884167eafadb700afcb97a0c3a83d
Bruno Martins <83421299+bruam@users.noreply.github.com>
2021-05-25 16:33:09 -0300

    Update UsuarioController.java
    Versão final do UsuarioController

--- src/main/java/unidac/unidac/cafe/controller/UsuarioController.java
+++ src/main/java/unidac/unidac/cafe/controller/UsuarioController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import unidac.unidac.cafe.model.*;
 
 import unidac.unidac.cafe.repository.UsuarioRepository;
 
@@ -14,11 +17,18 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioRepository repository;
 	
+	// Metodo de consulta
 	@GetMapping(path = "/api/usuario/{codigo}")
 	public ResponseEntity consultar(@PathVariable("codigo") Integer codigo) {
-		return repository.findById(codigo)
-				.map(record -> ResponseEntity.ok().body(record))
-				.orElse(ResponseEntity.notFound().build());
+		return repository.findById(codigo) // se achar no repositorio pelo código
+				.map(record -> ResponseEntity.ok().body(record)) // monta o corpo da requisição com o registro
+				.orElse(ResponseEntity.notFound().build()); // se não retorna notFound e faz build
+	}
+	
+	// Metodo para salvar no banco
+	@PostMapping(path = "/api/usuario/salvar")
+	public Usuario salver(@RequestBody Usuario usuario) {
+		return repository.save(usuario);
 	}
 
 }
